c3c52a566beff7db56bf1125f009fffcc951cef8
Wise E <dradazer@users.noreply.github.com>
2016-12-17 19:49:16 +0000

    Timer and randomisation added

--- MainClass.java
+++ MainClass.java
@@ -1,17 +1,40 @@
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.Random;
 
 // DESCRIPTION: Brute force approach to the subset sum problem.
 // There could be an exponential number of subsets, and this method attempts to search
 // for all of them. Meaning this algorithm is inefficient and exponential time is likely.
 
-public class MainClass {
+public class Exhaustive {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 34, 19, 27 };
-		int sum = 56;
-		findSubsets(arr, 0, sum, new Stack<Integer>());
-
+		//int[] arr = { 10, 34, 19, 27 };
+                Random r = new Random();
+                //change length here ↓
+                int[] set = new int[10];
+                int length = set.length;
+                
+                //smallest element in the array
+                int min = 1;
+                //largest element in the array
+                int max = 10;
+                
+                for (int i=0; i < length; i++)
+                {
+                    set[i] = (int) (r.nextInt(max - min + 1) + min);
+                }
+                System.out.println("Array: " + Arrays.toString(set));
+                //randomly generate target in range of min to max
+		int sum = r.nextInt(max - min + 1) + min;
+                System.out.println("Target: " + sum);
+                
+                long startT = System.nanoTime();
+		findSubsets(set, 0, sum, new Stack<Integer>());
+                long endT = System.nanoTime();
+                long duration = (endT - startT);
+                System.out.println(duration + " nanoseconds");
 	}
 
 	private static void findSubsets(int[] arr, int start, int target, Stack<Integer> s) {
