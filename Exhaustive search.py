d51c2495bf8422d5af723438ae1b42e8b6b053fd
Kenneth <cuyungk4@gmail.com>
2016-12-24 21:47:52 +0800

    Update Exhaustive search.py

--- Exhaustive search.py
+++ Exhaustive search.py
@@ -30,7 +30,7 @@ def display( stack ,target):
       for a in range (len(powersert(s))):
             line = (powersert(s))[a]
             total= sum((powersert(s))[a])
-            print "substring %s  Total: %s"%(line,total)
+            #print "substring %s  Total: %s"%(line,total)
             if (total == target):
                   print "\n\nThe answers is below: "
                   print "substring %s  Total: %s"%(line,total)
@@ -41,6 +41,38 @@ def display( stack ,target):
 
 #s = [12, 24, 52, 90, 56, 23, 10]
 #t = 134
+
+####test variables####
+#length of test; run the test ___ times
+lenTest = 50
+#list containng test execution time results
+arrTest = [0] * lenTest
+#average variable
+avgTest = 0
+#sum of list
+sumTest = 0
+####test variables####
+
+#test loop
+for i in range(0, lenTest, 1):
+    #create(list length, element min size,element max size)
+    c = create(10,1, 101)
+    # array is the first element of the return value
+    s = c[0]
+    # target is the srcond element of the return value
+    t = c[1]
+    Tstart = time.time()
+    display(s,t)
+    arrTest[i] = time.time() - Tstart
+    print "\nNext\n"
+
+#summing of results
+sumTest = sum(arrTest)
+#averaging the results
+avgTest = sumTest / lenTest
+print "Average time to complete ", lenTest, " algorithm runs: ", avgTest, " seconds"
+
+'''     
 #Generate the array      
 c = create(10,1, 101)
 # array is the first element of the return value
@@ -49,4 +81,7 @@ s = c[0]
 t = c[1]
 Tstart = time.time()
 display(s,t)
-print "The algorithm finish in %s seconds."%(time.time() - Tstart)      
+print "The algorithm finish in %s seconds."%(time.time() - Tstart)       
+'''     
+
+
